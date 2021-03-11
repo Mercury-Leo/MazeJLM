f68523f41e830673d2719e87994eda9493e688f5
Mercury <Kansamas@gmail.com>
2021-03-11 11:45:31 +0200

    Prototype
    Added:
    Basic maze layout for prototype jam.
    Cinemachine added: Will detect maze area and close up to character.
    Joystick UI, using new input system.
    Volume lighting for some atmosphere.
    World edge - will detect if player is off the plane and restart scene.

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class CameraLook : MonoBehaviour
+{
+
+    [SerializeField]
+    float lookSpeed = 2f;
+
+    private CinemachineFreeLook cinemachine;
+    private Player playerInput;
+
+    private void Awake()
+    {
+        playerInput = new Player();
+        cinemachine = GetComponent<CinemachineFreeLook>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+
+    void Update()
+    {
+
+        Vector2 delta = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+        // Debug.Log("detla x: " + delta.x + "\n delta y: " + delta.y);
+        cinemachine.m_XAxis.Value += delta.x * lookSpeed * Time.deltaTime;
+        cinemachine.m_YAxis.Value += delta.y * lookSpeed * Time.deltaTime + 50f;
+
+    }
+}
---
+++ Assets/Scripts/Inputs/Player.cs
@@ -0,0 +1,216 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Inputs/Player.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Player : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Player()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMain"",
+            ""id"": ""bac89bf2-a9d9-4112-a43e-57339cb93ddd"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""b3a557dd-5385-4571-96cc-e471cda398f9"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""dc4211b9-f66e-4733-9f5c-9b2c4560e99d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""e0311528-c1a7-4af0-8bab-05ec7e071f29"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""b3eebdd7-f02a-461f-8767-6fef23238c31"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""5ab090a6-bc46-49d6-94a8-6f9a9d64085e"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""b7eeb416-4686-4a12-ba76-7773241c4c55"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""58e507cf-7a64-46dc-a7a5-156c6be5b441"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""ee795f8d-0569-455f-926f-9a7fa9f26d8e"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9196ed5c-8098-410a-acdd-a6ccbeff310c"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMain
+        m_PlayerMain = asset.FindActionMap("PlayerMain", throwIfNotFound: true);
+        m_PlayerMain_Move = m_PlayerMain.FindAction("Move", throwIfNotFound: true);
+        m_PlayerMain_Jump = m_PlayerMain.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMain
+    private readonly InputActionMap m_PlayerMain;
+    private IPlayerMainActions m_PlayerMainActionsCallbackInterface;
+    private readonly InputAction m_PlayerMain_Move;
+    private readonly InputAction m_PlayerMain_Jump;
+    public struct PlayerMainActions
+    {
+        private @Player m_Wrapper;
+        public PlayerMainActions(@Player wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerMain_Move;
+        public InputAction @Jump => m_Wrapper.m_PlayerMain_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMain; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMainActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMainActions instance)
+        {
+            if (m_Wrapper.m_PlayerMainActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerMainActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerMainActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerMainActions @PlayerMain => new PlayerMainActions(this);
+    public interface IPlayerMainActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
